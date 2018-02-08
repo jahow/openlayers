165bde781ba3d0dbcfa9577658c6d04963121469
Olivier Guyot <olivier.guyot@camptocamp.com>
2018-02-08 13:18:32 +0100

    modified osm & mapbox VT examples to use webgl

--- examples/mapbox-vector-tiles.js
+++ examples/mapbox-vector-tiles.js
@@ -31,5 +31,6 @@ const map = new Map({
   view: new View({
     center: [0, 0],
     zoom: 2
-  })
+  }),
+  renderer: 'webgl'
 });
--- examples/vector-layer.js
+++ examples/vector-layer.js
@@ -46,7 +46,8 @@ const map = new Map({
   view: new View({
     center: [0, 0],
     zoom: 1
-  })
+  }),
+  renderer: 'webgl'
 });
 
 const highlightStyle = new Style({
