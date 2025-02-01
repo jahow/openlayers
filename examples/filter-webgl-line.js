365bdea65f1b6c0cb579d8c6e9e40a14a840bb9f
Olivia Guyot <olivia.guyot@camptocamp.com>
2025-02-01 16:03:13 +0100

    fix filter-webgl-line example which used the wrong style format

--- examples/filter-webgl-line.js
+++ examples/filter-webgl-line.js
@@ -6,19 +6,23 @@ import WebGLVectorLayer from '../src/ol/layer/WebGLVector.js';
 import OSM from '../src/ol/source/OSM.js';
 import VectorSource from '../src/ol/source/Vector.js';
 
-const lineStyle = {
-  'stroke-width': 4,
-  'stroke-color': [
-    'interpolate',
-    ['linear'],
-    ['line-metric'],
-    1303200000,
-    'hsl(312,100%,39%)',
-    1303240000,
-    'hsl(36,100%,45%)',
-  ],
-  filter: ['<=', ['line-metric'], ['var', 'timestamp']],
-};
+const lineStyle = [
+  {
+    style: {
+      'stroke-width': 4,
+      'stroke-color': [
+        'interpolate',
+        ['linear'],
+        ['line-metric'],
+        1303200000,
+        'hsl(312,100%,39%)',
+        1303240000,
+        'hsl(36,100%,45%)',
+      ],
+    },
+    filter: ['<=', ['line-metric'], ['var', 'timestamp']],
+  },
+];
 
 const igcUrls = [
   'data/igc/Clement-Latour.igc',
